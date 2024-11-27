9755cb36e8adb8ca6c208777974b92225069b52b
Tinsae Giblin <tinsae.giblin@gmail.com>
2024-11-26 22:42:14 -0800

    Assignment 8

--- blogging/views.py
+++ blogging/views.py
@@ -1,39 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from blogging.models import Post
 
-'''def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain") '''
+# Class-based ListView for displaying posts
+class PostListView(ListView):
+    template_name = "blogging/list.html"
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
 
-'''def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")'''
+# Class-based DetailView for displaying a single post
+class PostDetailView(DetailView):
+    template_name = "blogging/detail.html"
+    model = Post
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
-
-# Create your views here.
+    def get_queryset(self):
+        # Filter only published posts
+        return Post.objects.exclude(published_date__exact=None)
